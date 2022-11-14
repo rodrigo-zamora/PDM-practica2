39bf9f1b3620d4512eaf7787811186d21cde9f95
Rodrigo Zamora <71241681+rodrigo-zamora@users.noreply.github.com>
2022-11-14 12:47:08 -0600

    Login page

--- lib/music/login_page.dart
+++ lib/music/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 import 'auth_bloc/auth_bloc.dart';
 
@@ -8,21 +9,88 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'Music App',
-              style: TextStyle(fontSize: 40),
+      body: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/login.gif'),
+                fit: BoxFit.cover,
+              ),
             ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {},
-              child: Text('Sign in with Google'),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(top: 32.0),
+                      child: Text(
+                        'Sign In',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 36,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 64,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      height: MediaQuery.of(context).size.height / 6,
+                      child: Image.asset(
+                        'assets/music.png',
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 256,
+                ),
+                Row(
+                  children: [
+                    Container(
+                      alignment: Alignment.center,
+                      width: MediaQuery.of(context).size.width - 32,
+                      child: MaterialButton(
+                        color: Colors.green,
+                        onPressed: () {
+                          print('Google');
+                        },
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Padding(
+                              padding:
+                                  const EdgeInsets.only(left: 16.0, right: 16),
+                              child: FaIcon(
+                                FontAwesomeIcons.google,
+                                color: Colors.white,
+                                size: 18,
+                              ),
+                            ),
+                            Text(
+                              'Iniciar con Google',
+                              style: TextStyle(color: Colors.white),
+                            ),
+                          ],
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ],
             ),
-          ],
-        ),
+          )
+        ],
       ),
     );
   }
